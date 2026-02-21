c9bad880df3e46cfeb7d112c4ea13ff6750bb899
RaviKishore Sara <ravikishoresara340@gmail.com>
2026-02-21 13:33:12 +0530

    addressing the syntax errors

--- Roboshop/roboshop.sh
+++ Roboshop/roboshop.sh
@@ -2,36 +2,32 @@
 
 SG_ID="sg-04d26ebef6a9e9ddc"
 AMI_ID="ami-0220d79f3f480ecf5"
-MangoDb="MangoDb"
 
-
-for instance in $@
+for instance in "$@"
 do 
-instance_ID=$(aws ec2 run-instances \
---image-id $AMI_ID \
---instance-type "t3.micro" \
---security-group-ids $SG_ID \ 
---tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \ 
---query 'Instances[0].InstanceId' \
---output text)
+    instance_ID=$(aws ec2 run-instances \
+        --image-id "$AMI_ID" \
+        --instance-type "t3.micro" \
+        --security-group-ids "$SG_ID" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+        --query 'Instances[0].InstanceId' \
+        --output text)
 
-if [ $instance == "frontend" ]; then
-    IP = $(
-        aws ec2 describe-instances \
-        --instance-ids $instance_id \
-        --query 'Reservations[].Instances[].PublicIpAddress' \
-        --output text
-    )
-else
-    IP=$(
-        ws ec2 describe-instances \
-        --instance-ids $instance_id \
-        --query 'Reservations[].Instances[].PublicIpAddress' \
-        --output text
-    )
-fi
+    if [ "$instance" == "frontend" ]; then
+        IP=$(aws ec2 describe-instances \
+            --instance-ids "$instance_ID" \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+            --output text)
+    else
+        IP=$(aws ec2 describe-instances \
+            --instance-ids "$instance_ID" \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+            --output text)
+    fi
 
-echo "IP Adress: " 
-done 
+    echo "Instance: $instance"
+    echo "Instance ID: $instance_ID"
+    echo "IP Address: $IP"
+    echo "-----------------------------"
 
-
+done
